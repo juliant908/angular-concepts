933b3914280e1500744368ea73b70cbe956bbd24
Julian <juliant908@gmail.com>
2024-07-03 19:22:11 -0500

    Updated packages and created NgRx component

---
+++ src/app/advanced-concepts/advanced-concepts/ngrx/ngrx.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-ngrx',
+  standalone: true,
+  imports: [],
+  templateUrl: './ngrx.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NgrxComponent {
+
+}
